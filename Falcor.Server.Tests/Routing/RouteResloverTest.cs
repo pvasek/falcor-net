ffa64a13f7df48ce606af1b323273ea7b861ebb3
Pavel Vasek <pvasek@gmail.com>
2015-05-17 18:18:07 +0200

    route resolver tests added

--- Falcor.Server.Tests/Routing/RouteResloverTest.cs
+++ Falcor.Server.Tests/Routing/RouteResloverTest.cs
@@ -9,7 +9,7 @@ namespace Falcor.Server.Tests.Routing
     public class RouteResloverTest
     {
         [Test]
-        public void Should_match_simple_route()
+        public void Should_match_route_with_index()
         {
             var route1 = new Route(new PropertiesPathComponent("events"));
             var route2 = new Route(new PropertiesPathComponent("users"), new RangePathComponent());
@@ -25,6 +25,67 @@ namespace Falcor.Server.Tests.Routing
 
             Assert.AreEqual(1, result.Count);
             Assert.AreEqual(route3, result[0]);
+        }
+
+        [Test]
+        public void Should_match_single_property_with_list_of_properties()
+        {
+            var route1 = new Route(new PropertiesPathComponent("events"), new IndexPathComponent(), new PropertiesPathComponent("name"));
+            var route2 = new Route(new PropertiesPathComponent("events"), new IndexPathComponent(), new PropertiesPathComponent("from", "to"));
+            var route3 = new Route(new PropertiesPathComponent("users"), new IndexPathComponent());
+
+            var target = new RouteResolver(new[] { route1, route2, route3 });
+            var result = target.FindRoute(new IPathComponent[]
+                {
+                    new PropertiesPathComponent("events"),
+                    new IndexPathComponent(),
+                    new PropertiesPathComponent("from"), 
+                })
+                .ToList();
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(route2, result[0]);
+        }
+
+        [Test]
+        public void Should_match_list_of_properties_with_list_of_properties()
+        {
+            var route1 = new Route(new PropertiesPathComponent("events"), new IndexPathComponent(), new PropertiesPathComponent("name"));
+            var route2 = new Route(new PropertiesPathComponent("events"), new IndexPathComponent(), new PropertiesPathComponent("from", "to"));
+            var route3 = new Route(new PropertiesPathComponent("users"), new IndexPathComponent());
+
+            var target = new RouteResolver(new[] { route1, route2, route3 });
+            var result = target.FindRoute(new IPathComponent[]
+                {
+                    new PropertiesPathComponent("events"),
+                    new IndexPathComponent(),
+                    new PropertiesPathComponent("to", "from"), 
+                })
+                .ToList();
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(route2, result[0]);
+        }
+
+        [Test]
+        public void Should_match_list_of_properties_with_list_of_properties_and_simple_property()
+        {
+            var route1 = new Route(new PropertiesPathComponent("events"), new IndexPathComponent(), new PropertiesPathComponent("name"));
+            var route2 = new Route(new PropertiesPathComponent("events"), new IndexPathComponent(), new PropertiesPathComponent("from", "to"));
+            var route3 = new Route(new PropertiesPathComponent("users"), new IndexPathComponent());
+
+            var target = new RouteResolver(new[] { route1, route2, route3 });
+            var result = target.FindRoute(new IPathComponent[]
+                {
+                    new PropertiesPathComponent("events"),
+                    new IndexPathComponent(),
+                    new PropertiesPathComponent("to", "name"), 
+                })
+                .ToList();
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(route1, result[0]);
+            Assert.AreEqual(route2, result[1]);
         }
     }
 }
